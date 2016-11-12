cf9fcd4d39ff88adbf7845e82951eba48f41207d
jazzyg <vogue@rediffmail.com>
2016-11-12 11:22:25 -0500

    added response object

--- WCFServiceWebRole/serverResponse.cs
+++ WCFServiceWebRole/serverResponse.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
+
 
 namespace WCFServiceWebRole
 {
+    [DataContract]
     public class serverResponse
     {
+        public string Status;
+        public NotesData note { get; set; }
     }
 }
